43650ef9efcdc910240f9fdf5f8eeabd7ac5222d
wei <begining1003@gmail.com>
2012-07-11 01:03:19 +0800

    新增動態打點元件
    透過setIcon傳入所需圖片並設定縮放及旋轉，
    再透過setPosition傳入打點的X軸及Y軸，
    為了確保每個行動裝置打點位置都會相同，
    請再傳入開發的行動裝置解析度。

---
+++ src/edu/stu/mobile/util/DynamicImageView.java
@@ -0,0 +1,61 @@
+package edu.stu.mobile.util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class DynamicImageView extends ImageView {
+	private Matrix matrix = null;
+	private Context context = null;
+	private String Tag = "DynamicImageView";
+
+	/**
+	 * 初始化ImageView
+	 * 
+	 * @param context 初始化作用
+	 * @author Webber
+	 */
+	public DynamicImageView(Context context) {
+		super(context);
+		DynamicImageView.this.context = context;
+	}
+
+	/**
+	 * 設定圖片的位置，左上角為(0,0)，X越大越是往右邊移動，Y越大則往下移動
+	 * 
+	 * @param seatX icon_X
+	 * @param seatY icon_Y
+	 * @param HeightPixelsDevelop 開發者手機解析度(高)
+	 * @param WidthPixelsDevelop 開發者手機解析度(寬)
+	 */
+	public void setPosition(int seatX, int seatY, int HeightPixelsDevelop, int WidthPixelsDevelop) {
+		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		params.setMargins(
+				(int) (ResolutionUtils.getWidthPixels(this.context) / HeightPixelsDevelop * seatX),
+				(int) (ResolutionUtils.getHeightPixels(this.context) / WidthPixelsDevelop * seatY), 0, 0);
+		DynamicImageView.this.setLayoutParams(params);
+	}
+
+	/**
+	 * 縮放比例如果是 "1"，為圖片本身大小，數字越小圖片即越小。 旋轉如果是 "0"，即為不旋轉，正數為向右邊轉。
+	 * 
+	 * @param icon 所顯示之圖案
+	 * @param ScaleHeight 縮放比例(高)
+	 * @param ScaleWidth 縮放比例(寬)
+	 * @param Rotate 旋轉
+	 * @author webber
+	 */
+	public void setIcon(Bitmap icon, float ScaleHeight, float ScaleWidth, float Rotate) {
+		if (matrix == null) {
+			matrix = new Matrix();
+		}
+		matrix.postScale(ScaleHeight, ScaleWidth);
+		matrix.postRotate(Rotate);
+		Bitmap dstbmp = Bitmap.createBitmap(icon, 0, 0, icon.getWidth(), icon.getHeight(), matrix, true);
+		DynamicImageView.this.setImageBitmap(dstbmp);
+	}
+
+}

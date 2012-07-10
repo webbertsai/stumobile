7590fe26f0f33eeb0f3bd2b1db7bb60de2b1ac77
wei <begining1003@gmail.com>
2012-07-11 00:26:15 +0800

    改變getPixels的傳入參數

---
+++ src/edu/stu/mobile/util/ResolutionUtils.java
@@ -0,0 +1,30 @@
+package edu.stu.mobile.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+public class ResolutionUtils {
+	/**
+	 * 取得螢幕解析度的寬
+	 * @param activity 初始化用
+	 * @return 螢幕解析度的寬
+	 * @author Webber
+	 */
+	public static int getWidthPixels(Context context) {
+		DisplayMetrics dm = new DisplayMetrics();
+		((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(dm);
+		return dm.widthPixels;
+	}
+	/**
+	 * 取得螢幕解析度的高
+	 * @param activity 初始化用
+	 * @return 螢幕解析度的高
+	 * @author Webber	
+	 */
+	public static int getHeightPixels(Context context) {
+		DisplayMetrics dm = new DisplayMetrics();
+		((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(dm);
+		return dm.heightPixels;
+	}
+}

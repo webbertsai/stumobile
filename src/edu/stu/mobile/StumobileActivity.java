dbff0674ee718b0029119ab3cb37d9206df9bdbf
wei <begining1003@gmail.com>
2012-07-10 14:36:14 +0800

    將package name edu.stu 改為 edu.stu.mobile

---
+++ src/edu/stu/mobile/StumobileActivity.java
@@ -0,0 +1,22 @@
+package edu.stu.mobile;
+
+import edu.stu.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class StumobileActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.index);
+
+	}
+
+	public void NewStu(View v) {
+		startActivity(new Intent(StumobileActivity.this, edu.stu.NewStudents.NewStudentsMain.class));
+	}
+
+}

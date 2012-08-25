613ce0b71fd3f1d46fdf8ae07f99a3059303e41a
webber <begining1003@gmail.com>
2012-08-26 06:17:04 +0800

    新增主頁面中的小功能選項

---
+++ src/edu/stu/mobile/MainItem.java
@@ -0,0 +1,50 @@
+package edu.stu.mobile;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.RadioGroup.LayoutParams;
+import android.widget.TextView;
+
+public class MainItem extends BaseAdapter {
+	private LayoutInflater inflater;
+	private ArrayList<HashMap<String, Object>> data;
+	private double errorW, errorH;
+
+	MainItem(Context context, ArrayList<HashMap<String, Object>> data, double errorW, double errorH) {
+		inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		this.data = data;
+		this.errorH = errorH;
+		this.errorW = errorW;
+	}
+
+	public int getCount() {
+		return data.size();
+	}
+
+	public Object getItem(int position) {
+		return null;
+	}
+
+	public long getItemId(int position) {
+		return data.size();
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View view = inflater.inflate(R.layout.main_item, null);
+		if (data != null && data.size() > 0) {
+			((View) view.findViewById(R.id.icon)).setBackgroundResource((Integer) data.get(position).get("icon"));
+			((View) view.findViewById(R.id.icon)).setLayoutParams(new LayoutParams((int) this.errorH * 75, (int) errorW * 75));
+			((TextView) view.findViewById(R.id.title)).setText((String) data.get(position).get("title"));
+
+		}
+
+		return view;
+	}
+
+}
--- src/edu/stu/mobile/main.java
+++ src/edu/stu/mobile/main.java
@@ -1,23 +1,69 @@
 package edu.stu.mobile;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import edu.stu.mobile.util.ResolutionUtils;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
 public class Main extends Activity {
 
-	// Declaration
 	TextView About;
 	GridView FunctionMenu;
+	LinearLayout PageNum;
+
+	ArrayList<HashMap<String, Object>> FunctionMenuData;
+
+	private void setFunctionMenuData() {
+
+		// 各功能Icon
+		int FunctionMenuIcon[] = {
+				R.drawable.ext_table, R.drawable.icon03, R.drawable.icon03, R.drawable.icon03, R.drawable.icon03, R.drawable.icon03, R.drawable.icon03, R.drawable.icon03, R.drawable.icon03
+		};
+		// 個功能名稱 （建議使用 Strings.xml 新增，以便多國語系使用）
+		String FunctionMenuText[] = {
+				getString(R.string.ext_table), "test", "test", "test", "test", "test", "test", "test", "test"
+		};
+
+		// icon 與 title 相同數量才會新增
+		if (FunctionMenuText.length == FunctionMenuIcon.length) {
+			for (int i = 0; i < 8; i++) {
+				HashMap<String, Object> index = new HashMap<String, Object>();
+				index.put("title", FunctionMenuText[i]);
+				index.put("icon", FunctionMenuIcon[i]);
+				FunctionMenuData.add(index);
+			}
+		}
+
+		for (int i = 0; i < (FunctionMenuIcon.length - 1) / 8 + 1; i++) {
+			View dot = new View(this);
+			dot.setBackgroundResource(R.drawable.dot_black);
+			LayoutParams lp = new LayoutParams(10, 10);
+			lp.setMargins(5, 0, 5, 0);
+			dot.setLayoutParams(lp);
+			PageNum.addView(dot);
+		}
+
+	};
 
 	private void findViews() {
 		About = (TextView) findViewById(R.id.About);
 		FunctionMenu = (GridView) findViewById(R.id.FunctionMenu);
+		PageNum = (LinearLayout) findViewById(R.id.FunctionMenuNumber);
 	}
 
 	private void init() {
 		About.setText(getString(R.string.stu_name) + " | " + getString(R.string.stu_zipcode) + getString(R.string.stu_address) + " | TEL:" + getString(R.string.stu_phnoe));
+		FunctionMenuData = new ArrayList<HashMap<String, Object>>();
+		setFunctionMenuData();
+
 	}
 
 	@Override
@@ -26,6 +72,7 @@ public class Main extends Activity {
 		setContentView(R.layout.main);
 		findViews();
 		init();
+		FunctionMenu.setAdapter(new MainItem(this, FunctionMenuData, ResolutionUtils.getPixelsHeight(this) / 800, ResolutionUtils.getPixelsHeight(this) / 480));
 	}
 
 }

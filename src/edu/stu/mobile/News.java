605e15afa20455e41882ff4704c3c691cf7d5367
wei <begining1003@gmail.com>
2012-09-14 02:15:27 +0800

    新增最新消息 (layout)

---
+++ src/edu/stu/mobile/News.java
@@ -0,0 +1,11 @@
+package edu.stu.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class News extends Activity{
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.news);
+	}
+}

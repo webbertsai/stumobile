42a3212a15a39c6745d3f65f5542451fb79a1e19
wei <begining1003@gmail.com>
2012-11-15 18:18:44 +0800

    add Ext layout and Activity

---
+++ src/edu/stu/mobile/Ext.java
@@ -0,0 +1,12 @@
+package edu.stu.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Ext extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ext);
+	}
+}

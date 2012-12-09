726c3763202710ab26bdeeec212393c266152d51
wei <begining1003@gmail.com>
2012-12-10 04:50:10 +0800

    add NewsBrowse Activity

---
+++ src/edu/stu/mobile/NewsBrowse.java
@@ -0,0 +1,11 @@
+package edu.stu.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewsBrowse extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.news_browse);
+	}
+}
